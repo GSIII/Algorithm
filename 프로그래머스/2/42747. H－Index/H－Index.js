1cdbbeb5d5bd991aa48ccdf832c65c45674d1722
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-09-26 11:59:20 +0900

    [level 2] Title: H-Index, Time: 0.60 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,11 +1,11 @@
 function solution(citations) {
-    citations.sort((a,b)=>b-a)
+    citations.sort((a,b)=>b-a);
     let count = 0;
-    let i;
-    for (i=0;i<citations.length+1;i++) {
-        if (citations[i]>=i+1) {
+    for (let i=0;i<citations.length;i++) {
+        if (i<citations[i]) {
             count++
         }
     }
     return count
+    
 }
