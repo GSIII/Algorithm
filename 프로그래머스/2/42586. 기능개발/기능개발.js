f43abbf21c5ebd9696491c013fc2d81675789001
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-09-26 12:12:26 +0900

    [level 2] Title: 기능개발, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,4 +1,19 @@
 function solution(progresses, speeds) {
-    var answer = [];
-    return answer;
+    let answer = new Map();
+    let workSpeed = [];
+    let result = [];
+    for (let i=0;i<progresses.length;i++) {
+        let speed = Math.ceil((100-progresses[i])/speeds[i])
+        workSpeed.push(speed)
+    }
+    for (let j=0;j<workSpeed.length-1;j++) {
+        if (workSpeed[j]>workSpeed[j+1]) {
+            workSpeed[j+1] = workSpeed[j]
+        }
+    }
+    for (let k=0;k<workSpeed.length;k++) {
+        answer.set(workSpeed[k],(answer.get(workSpeed[k])||0)+1)
+    }
+    answer.forEach(count=>result.push(count))
+    return result
 }
