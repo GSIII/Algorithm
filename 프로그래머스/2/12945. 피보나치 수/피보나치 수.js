5b291c001ece38803f5d4db112a3ae1c011575ed
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-09-30 19:38:36 +0900

    [level 2] Title: 피보나치 수, Time: 2.41 ms, Memory: 36.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let a=0;
+    let b=1;
+    let temp
+    for (let i=2;i<=n;i++) {
+        temp = (a+b)%1234567;
+        a=b;
+        b = temp
+    }
+    return temp
+}
