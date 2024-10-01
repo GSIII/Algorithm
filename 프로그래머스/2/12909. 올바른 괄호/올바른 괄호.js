22168b9044ad3d11f64f138f196b676c7951960d
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-10-01 22:44:27 +0900

    [level 2] Title: 올바른 괄호, Time: 2.72 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -2,15 +2,15 @@ function solution(s){
     let count = 0;
     for (let i=0;i<s.length;i++) {
         if (s[i]==='(') {
-            count++;
+            count++
         } else {
             count--;
             
             if (count<0) {
                 return false
-            }
+            } 
         }
-    }  
+    }
     if (count===0) {
         return true
     } else {
