34f8b1d8958e6da12a42e76fe2bbc437b583bf8a
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-10-02 13:58:38 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 2.98 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,16 +1,12 @@
 function solution(s) {
-    let transformations = 0;  // 이진 변환의 횟수
-    let removedZeros = 0;  // 제거된 0의 개수
-    
-    while (s!=='1') {
-        let zeroCount = s.split('0').length-1
-        removedZeros += zeroCount;
+    let countDigit = 0;
+    let countRemove = 0;
+    while(s !== '1') {
+        let countZero = s.split('0').length-1;
+        countRemove += countZero
         
-        s = s.replace(/0/g,'');
-        let sLength = s.length;
-        
-        s = sLength.toString(2)
-        transformations++
+        s = (s.length-countZero).toString(2)
+        countDigit++
     }
-    return [transformations,removedZeros]
+    return [countDigit,countRemove]
 }
