88d5ff6ace5183224373d0448fdff66955c8301a
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-10-04 22:09:44 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,12 +1,11 @@
-// 피보나치
 function solution(n) {
-    let a = 1;
-    let b = 2;
+    let n1 = 1;
+    let n2 = 2;
     let temp;
     for (let i=3;i<=n;i++) {
-        temp = (a+b)%1234567;
-        a=b;
-        b=temp;
+        temp = (n1+n2)%1234567;
+        n1 = n2;
+        n2 = temp;
     }
     if (n===1) {
         return 1
