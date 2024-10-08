136b1c5d2d20a005d7f641d7be6cbf38d541ce09
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-10-08 12:55:52 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,17 +1,17 @@
 function solution(n, words) {
-    let usedWords = new Set();
+    let newWords = new Set();
     
-    usedWords.add(words[0])
+    newWords.add(words[0]);
     
-    for (let i=1;i<=words.length-1;i++) {
-        let currentWord = words[i];
+    for (let i=1;i<words.length;i++) {
+        let currentWord = words[i]
         let previousWord = words[i-1];
-        if (previousWord[previousWord.length-1]!==currentWord[0] || usedWords.has(currentWord)) {
-            let person = (i%n)+1;
+        if (currentWord[0]!==previousWord[previousWord.length-1] || newWords.has(currentWord)) {
+            let person = (i%n) +1;
             let turn = Math.floor(i/n)+1
             return [person,turn]
         }
-        usedWords.add(currentWord)
+        newWords.add(currentWord)
     }
     return [0,0]
 }
