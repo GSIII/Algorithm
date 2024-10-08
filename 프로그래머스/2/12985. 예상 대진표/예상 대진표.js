2d5183df4e2d46695a13ebba78b9a72ca6ba1bfd
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-10-08 12:57:29 +0900

    [level 2] Title: 예상 대진표, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,16 +1,10 @@
 function solution(n,a,b)
 {
     let count = 0;
-
-    while (a!==b) {
- 
-        a = Math.ceil(a / 2);
-        b = Math.ceil(b / 2);
-
-        count++;
-
-        
+    while(a!==b) {
+        a = Math.ceil(a/2)
+        b = Math.ceil(b/2)
+        count++
     }
-    
-    return count;
+    return count
 }
