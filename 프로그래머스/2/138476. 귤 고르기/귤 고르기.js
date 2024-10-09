45b1af786e2fead428e5726b43bf453f5a40e7f6
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-10-09 14:02:02 +0900

    [level 2] Title: 귤 고르기, Time: 31.95 ms, Memory: 41.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,21 +1,22 @@
 function solution(k, tangerine) {
     let tangerineMap = new Map();
-    tangerine.forEach(size=>{
-        tangerineMap.set(size, (tangerineMap.get(size)||0)+1)
+    
+    tangerine.forEach((num)=>{
+        tangerineMap.set(num,(tangerineMap.get(num)||0)+1)
     })
     
-    let arr = Array.from(tangerineMap.values()).sort((a, b) => b - a);
-
-    let selectedNum = 0;
-    let count = 0;
     
-    for (let i=0;i<arr.length;i++) {
-        selectedNum += arr[i]
+    let tangerineNum = Array.from(tangerineMap.values()).sort((a,b)=>b-a);
+    
+    let selectedNum = 0;
+    let count = 0
+    for (let i=0;i<tangerineNum.length;i++) {
+        k -= tangerineNum[i]
         count++
-        
-        if (selectedNum>=k) {
+        if (k<=0) {
             break;
         }
+        
     }
     return count
 }
