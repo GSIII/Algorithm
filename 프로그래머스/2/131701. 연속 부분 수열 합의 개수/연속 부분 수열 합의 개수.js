205e7d1d9301232c7e2e7142e46b145cdadc9e10
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-10-09 14:08:18 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1085.87 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,23 +1,16 @@
 function solution(elements) {
-    let n = elements.length;
+    let extendEl = elements.concat(elements); 
     
-    // 원형 수열을 처리하기 위해 elements 배열을 두 배로 확장
-    let extendedElements = elements.concat(elements);  // [7, 9, 1, 1, 4, 7, 9, 1, 1, 4]
+    let elementSet = new Set()
     
-    let uniqueSums = new Set();  // 중복을 제거하기 위한 Set
-    
-    // 부분 수열의 길이를 1부터 n까지 증가시키며 모든 경우의 합을 계산
-    for (let length = 1; length <= n; length++) {
-        // 시작 인덱스를 0부터 n-1까지 탐색
-        for (let start = 0; start < n; start++) {
+    for (let n=1; n<=elements.length;n++) {
+        for (let start=0;start<elements.length;start++) {
             let sum = 0;
-            // 연속된 length 길이의 부분 수열 합을 계산
-            for (let i = 0; i < length; i++) {
-                sum += extendedElements[start + i];  // 확장된 배열에서 연속된 부분 수열 합을 구함
+            for (let i=0;i<n;i++){
+                sum += extendEl[start+i]
             }
-            uniqueSums.add(sum);  // Set에 합을 추가 (중복 제거됨)
+            elementSet.add(sum)
         }
     }
-    
-    return uniqueSums.size;  // 고유한 합의 개수를 반환
+    return elementSet.size
 }
