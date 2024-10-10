4fd53bce7705dc7165379278fdb385cb0630265d
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-10-10 22:55:50 +0900

    [level 2] Title: 괄호 회전하기, Time: 17.38 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,37 +1,31 @@
 function solution(s) {
-    let arr = s.split('');
-    let result = 0;
-    
-    const isValid = (rotatedArr) => {
+    let arr = s.split('')
+    const isValid = (arr) =>{
         let stack = [];
-        for (let i = 0; i < rotatedArr.length; i++) {
-            // 여는 괄호일 경우 스택에 추가
-            if (rotatedArr[i] === '[' || rotatedArr[i] === '{' || rotatedArr[i] === '(') {
-                stack.push(rotatedArr[i]);
+        for (let i=0;i<arr.length;i++) {
+            if (arr[i]==='['||arr[i]==='{'||arr[i]==='(') {
+                stack.push(arr[i])
             } else {
-                if (stack.length === 0) return false;
+                if (stack.length===0) return false;
                 
                 let popEl = stack.pop();
                 
-                // 닫는 괄호와 스택에서 꺼낸 여는 괄호가 짝이 맞는지 확인
-                if ((rotatedArr[i] === ']' && popEl !== '[') ||
-                    (rotatedArr[i] === '}' && popEl !== '{') ||
-                    (rotatedArr[i] === ')' && popEl !== '(')) {
-                    return false;
+                if ((arr[i]===']'&&popEl!=='[') 
+                    ||(arr[i]==='}'&&popEl!=='{') 
+                    ||(arr[i]===')'&&popEl!=='(')) {
+                    return false 
                 }
             }
         }
-        // 스택이 비어있다면 모든 괄호가 짝이 맞음
-        return stack.length === 0;
-    };
-    
-    // 문자열을 회전하며 유효성 확인
-    for (let j = 0; j < s.length; j++) {
-        let rotatedArr = arr.slice(j).concat(arr.slice(0, j));
-        if (isValid(rotatedArr)) {
-            result++;
-        }
+        return stack.length===0
     }
     
-    return result;
+    let count = 0;
+    for (let i=0;i<s.length;i++) {
+        let rotateArr = arr.slice(i).concat(arr.slice(0,i))
+        if (isValid(rotateArr)) {
+            count++
+        }
+    }
+    return count
 }
