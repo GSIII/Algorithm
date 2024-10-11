58399d5e073c4fe9c07f2aab77e581c37e2318e7
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-10-11 21:22:43 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,8 +1,9 @@
 function solution(array, commands) {
-    let answer = [];
+    let answer = []
     for (let i=0;i<commands.length;i++) {
-        let sliceArr = array.slice(commands[i][0]-1,commands[i][1]).sort((a,b)=>a-b)
-        answer.push(sliceArr[commands[i][2] -1 ])
+        let sliceArr = array.slice(commands[i][0]-1,commands[i][1]).sort((a,b)=>a-b);
+        answer.push(sliceArr[commands[i][2]-1])
     }
     return answer
+
 }
