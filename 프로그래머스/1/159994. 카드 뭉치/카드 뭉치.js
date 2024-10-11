8c9f12565d1338461c03b138cf1528677b42c9de
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-10-11 20:46:15 +0900

    [level 1] Title: 카드 뭉치, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,14 @@
+function solution(cards1, cards2, goal) {
+    let index1 = 0;
+    let index2 = 0;
+    for (let i=0;i<goal.length;i++) {
+        if (index1<cards1.length && goal[i]===cards1[index1]) {
+            index1++
+        } else if (index2<cards2.length && goal[i]===cards2[index2]) {
+            index2++
+        } else {
+            return 'No'
+        }
+    }
+    return 'Yes'
+}
