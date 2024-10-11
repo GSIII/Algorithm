ed21ac0cc6c9dd9290dd75b02cc6b4c0a20c4932
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-10-11 19:52:58 +0900

    [level 1] Title: 콜라 문제, Time: 0.52 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,12 +1,10 @@
 function solution(a, b, n) {
-    let totalCoca = 0
+    let totalCoca = 0;
     while(n>=a) {
-        let exchangeCoca = Math.floor(n/a)*b;
-        let restCoca = n%a;
-        
-        totalCoca += exchangeCoca;
-        
-        n = exchangeCoca+restCoca
+        let exchangeCoca = Math.floor((n/a))*b;
+        let leftCoca = n%a;
+        totalCoca += exchangeCoca
+        n = exchangeCoca+leftCoca
     }
     return totalCoca
 }
