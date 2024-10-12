b8ba65a610ffd2a87761b753240da777cf422296
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-10-12 22:38:42 +0900

    [level 2] Title: 전화번호 목록, Time: 136.92 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,6 +1,7 @@
 function solution(phone_book) {
-    phone_book.sort()
+    phone_book.sort();
     for (let i=0;i<phone_book.length-1;i++) {
+        // i+1번째를 비교하고 있으므로 phone_book.length로 for문 길이 설정을 하면 참조할 값이 없어서 에러가 발생한다!
         if (phone_book[i+1].startsWith(phone_book[i])){
             return false
         }
