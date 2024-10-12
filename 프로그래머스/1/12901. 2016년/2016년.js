89abd45ad10df1bcd0cdcf32f07d85e7965b2a6e
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-10-12 22:22:47 +0900

    [level 1] Title: 2016년, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,12 +1,9 @@
 function solution(a, b) {
-    
     let daysOfWeek = ['FRI','SAT','SUN','MON','TUE','WED','THU'];
     let daysInMonth = [31,29,31,30,31,30,31,31,30,31,30,31];
-    let totalDays = b - 1; 
-    for (let i = 0; i < a - 1; i++) { // 1월부터 a-1월까지의 일수를 더함
-        totalDays += daysInMonth[i];
+    let totalDay = b-1;
+    for (let i=0;i<a-1;i++) {
+        totalDay += daysInMonth[i]
     }
-
-    return daysOfWeek[totalDays % 7]; // 요일 계산 후 결과 반환
-    
+    return daysOfWeek[totalDay%7]
 }
