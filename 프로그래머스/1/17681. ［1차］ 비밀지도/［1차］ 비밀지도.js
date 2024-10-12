08be0c0b805ee1014ba47efb53a9150f49d89de3
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-10-12 22:06:34 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,8 +1,7 @@
 function solution(n, arr1, arr2) {
-    let digit = '';
-    let arr = []
+    let arr = [];
     for (let i=0;i<n;i++) {
-       digit = (arr1[i]|arr2[i]).toString(2).padStart(n,0).replace(/0/g,' ').replace(/1/g,'#')
+        let digit = (arr1[i]|arr2[i]).toString(2).padStart(n,0).replace(/0/g,' ').replace(/1/g, '#')
         arr.push(digit)
     }
     return arr
