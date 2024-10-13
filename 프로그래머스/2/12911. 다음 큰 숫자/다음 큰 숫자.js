92f251b97c1e46e0576937dcfc03cce1532adb79
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-10-13 23:21:28 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,8 +1,10 @@
 function solution(n) {
-    const oneCount =(num)=> num.toString(2).split('1').length-1;
-    let nextNum = n+1;
-    while(oneCount(n) !== oneCount(nextNum)) {
-        nextNum++
+    const oneCount = (num) => {
+       return num.toString(2).split('1').length-1;
     }
-    return nextNum
+    let nNext = n+1
+    while (oneCount(n) !== oneCount(nNext)) {
+        nNext++
+    }
+    return nNext
 }
