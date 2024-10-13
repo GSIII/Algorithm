1283d3a0387c6bea1e5f378ac9ced31707b5f951
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-10-13 22:57:59 +0900

    [level 2] Title: 의상, Time: 1.70 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,9 +1,11 @@
 function solution(clothes) {
-    let clothMap = new Map();
+    let clothesMap = new Map()
     clothes.forEach(([name,type])=>{
-        clothMap.set(type,(clothMap.get(type) || 0)+1)
+        clothesMap.set(type,(clothesMap.get(type)||0)+1)
     })
     let answer = 1;
-    clothMap.forEach(count=>answer *= (count+1))
+    clothesMap.forEach((count)=>{
+        answer *= (count+1)
+    })
     return answer-1
 }
