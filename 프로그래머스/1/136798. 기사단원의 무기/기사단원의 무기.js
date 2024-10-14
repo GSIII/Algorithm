a7ab9c4c2c5593c080936749651f49440285c559
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-10-14 22:55:32 +0900

    [level 1] Title: 기사단원의 무기, Time: 14.08 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,24 +1,20 @@
 function solution(number, limit, power) {
+    let divisors = Array(number+1).fill(0);
     
-    let divisors = Array(number + 1).fill(0);
-
-    
-    for (let i = 1; i <= number; i++) {
-        for (let j = i; j <= number; j += i) {
-            divisors[j]++;
+    for (let i=1;i<=number;i++) {
+        for (let j=i;j<=number;j+=i){
+            divisors[j]++
         }
     }
-
-    let totalWeight = 0;
-
     
-    for (let i = 1; i <= number; i++) {
-        if (divisors[i] > limit) {
-            totalWeight += power;  
+    let total = 0;
+    for (let i=1;i<divisors.length;i++) {
+        if (divisors[i]>limit) {
+            total+=power;
         } else {
-            totalWeight += divisors[i];  
+            total+=divisors[i];
         }
     }
-
-    return totalWeight;
+    
+    return total
 }
