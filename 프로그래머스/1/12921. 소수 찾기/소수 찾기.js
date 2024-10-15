28e09a0fafb33e395b5d5f82bccec24b7bae91ad
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-10-16 00:10:36 +0900

    [level 1] Title: 소수 찾기, Time: 59.55 ms, Memory: 44.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let arr = Array(n+1).fill(true);
+    arr[0]=arr[1]=false;
+    
+    for (let i=2;i<=Math.sqrt(n);i++) {
+        if (arr[i]) {
+            for (let j=i*i;j<=n;j+=i) {
+                arr[j]=false
+            }
+        }
+    }
+    
+    return arr.filter(num=>num).length
+}
