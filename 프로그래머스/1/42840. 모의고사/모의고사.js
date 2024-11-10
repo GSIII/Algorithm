c625e7d0aeb99e9bf23c8db9355aa2d0971cf0ec
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-10 14:13:49 +0900

    [level 1] Title: 모의고사, Time: 25.06 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -6,17 +6,19 @@ function solution(answers) {
     let supo1Ans = 0;
     let supo2Ans = 0;
     let supo3Ans = 0;
-
-    for (let i = 0; i < answers.length; i++) {
-        if (supo1[i % supo1.length] === answers[i]) supo1Ans++;
-        if (supo2[i % supo2.length] === answers[i]) supo2Ans++;
-        if (supo3[i % supo3.length] === answers[i]) supo3Ans++;
+    
+    for (let i=0;i<answers.length;i++) {
+        if (supo1[i%supo1.length]===answers[i]) supo1Ans++
+        if (supo2[i%supo2.length]===answers[i]) supo2Ans++
+        if (supo3[i%supo3.length]===answers[i]) supo3Ans++
     }
-
-    const ansArr = [supo1Ans, supo2Ans, supo3Ans];
-    const maxScore = Math.max(...ansArr);
-
-    return ansArr
-        .map((score, index) => (score === maxScore ? index + 1 : null))
-        .filter((index) => index !== null);
+    
+    let answer = [supo1Ans,supo2Ans,supo3Ans];
+    const maxScore = Math.max(...answer)
+    
+    return answer.map((score,index)=>(score===maxScore?index+1:null))
+                 .filter((index)=>index!==null)
+    
+    
+    
 }
