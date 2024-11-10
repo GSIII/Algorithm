804c903cd3c5b35d8751670c0837816048c8a28a
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-10 13:57:49 +0900

    [level 3] Title: 입국심사, Time: 42.13 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.js
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -2,19 +2,19 @@ function solution(n, times) {
     let left = 1;
     let right = Math.max(...times) * n;
     let answer = right;
-    while (left<=right) {
-        let mid = Math.floor((left+right)/2);
+    
+    while(left<=right) {
         let people = 0;
-        
+        let mid = Math.floor((left+right)/2);
         for (let time of times) {
-            people += Math.floor(mid/time)
+            people += Math.floor(mid/time);
             if (people >= n) break;
         }
-        if (people >= n) {
+        if (people>=n) {
             answer = mid;
-            right = mid-1
+            right = mid-1;
         } else {
-            left = mid+1
+            left = mid+1;
         }
     }
     return answer
