3381a8c23d21d843dfb500cf0304eace0a0c688d
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-11 21:54:43 +0900

    [level 1] Title: 과일 장수, Time: 220.56 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,10 +1,10 @@
 function solution(k, m, score) {
-    score.sort((a,b)=>b-a)
-    let maxProfit = 0;
+    score.sort((a,b)=>b-a);
+    let bestProfit = 0;
     for (let i=0;i<=score.length-m;i+=m) {
         let box = score.slice(i,i+m);
-        let price = Math.min(...box)*m
-        maxProfit += price
+        let price = Math.min(...box) * m;
+        bestProfit += price
     }
-    return maxProfit
+    return bestProfit
 }
