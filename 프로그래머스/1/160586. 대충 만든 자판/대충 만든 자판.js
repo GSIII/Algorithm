633826c04c00998c97d1a0d3469950dae5b880ac
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-12 11:29:54 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.06 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -0,0 +1,25 @@
+function solution(keymap, targets) {
+    let minPress = {}
+    keymap.forEach((key,index)=>{
+        for (let i=0;i<key.length;i++) {
+            let char = key[i]
+            
+            if (!minPress[char] || minPress[char]>i+1) {
+                minPress[char] = i+1
+            }
+        }
+    })
+    
+    return targets.map((target)=>{
+            let presses = 0;
+
+            for (let i=0;i<target.length;i++) {
+                if (!minPress[target[i]]) return -1
+
+                presses += minPress[target[i]]
+            }
+
+            return presses
+        })
+
+}
