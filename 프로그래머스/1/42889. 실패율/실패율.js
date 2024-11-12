ef445121bb310c6cd6b7dcd2fe72a0aaf6102036
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-12 20:54:10 +0900

    [level 1] Title: 실패율, Time: 4921.04 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,20 +1,17 @@
 function solution(N, stages) {
-    let arr = [];
-    let map = new Map();
-    let totalUsers = stages.length;
+    let answer = []
+    let map = new Map()
     
     for (let i=1;i<=N;i++) {
-        let failed = stages.filter((stage)=>stage===i).length
-        map.set(i,{failed,total:stages.filter(stage=>stage>=i).length})
+        let fail = stages.filter(stage=>stage===i).length;
+        map.set(i,{fail,totalUser:stages.filter(stage=>stage>=i).length})
     }
     
     for (let i=1;i<=N;i++) {
-        let {failed,total} = map.get(i)
-        let failRate = failed===0 ? 0 : failed/total
-        arr.push({stage:i,failRate})
+        let {fail,totalUser} = map.get(i)
+        let failRate = fail===0 ? 0 : fail/totalUser
+        answer.push({i,failRate})
     }
-
-    arr.sort((a,b)=>b.failRate-a.failRate || a.stage-b.stage)
-    return arr.map(item=>item.stage)
-    
+    answer.sort((a,b)=>b.failRate-a.failRate || a.i-b.i)
+    return answer.map(el=>el.i)
 }
