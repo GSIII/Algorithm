3c9a1fef214dba4c1b8bc195b7605d9879e9e7b4
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-13 16:28:05 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 8.76 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,14 +1,13 @@
 function solution(s) {
-    let arr = [];
-    let map = new Map();
+    let answer = []
+    let map = new Map()
     for (let i=0;i<s.length;i++) {
         if (map.has(s[i])) {
-            arr.push(i-map.get(s[i]))
+            answer[i] = i-map.get(s[i])
         } else {
-            arr.push(-1)
+            answer.push(-1)
         }
         map.set(s[i],i)
-        
     }
-    return arr
+    return answer
 }
