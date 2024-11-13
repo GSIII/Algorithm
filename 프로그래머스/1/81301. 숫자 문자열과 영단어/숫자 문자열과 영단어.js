deae179f0cf4d935f9bd42d56acc9125f2bdc4f3
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-13 15:38:46 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,16 +1,16 @@
 function solution(s) {
-    let word = '';
-    let number = ''
     let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let number = '';
+    let word = ''
     for (let i=0;i<s.length;i++) {
         if (isNaN(s[i])) {
-            word += s[i];
+            word+=s[i];
             if (numbers.includes(word)) {
-                number += numbers.indexOf(word);
-                word = '';
+                number+=numbers.indexOf(word)
+                word=''
             }
-        }else {
-            number += s[i]
+        } else {
+            number+=s[i]
         }
     }
     return +number
