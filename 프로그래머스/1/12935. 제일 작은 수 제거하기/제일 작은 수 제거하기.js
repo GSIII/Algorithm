1bdeb649930f0de1f10d471a71648076fce82423
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-13 16:18:17 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.59 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,6 +1,5 @@
 function solution(arr) {
     if (arr.length<=1) return [-1]
-    arr.splice(arr.indexOf(Math.min(...arr)),1)
-    return arr
-    
+    let minNum = Math.min(...arr)
+    return arr.filter(num=>num!==minNum)
 }
