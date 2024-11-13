58758c5992dd7121916be327c6c4b7d50f68c7f8
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-13 19:21:34 +0900

    [level 1] Title: 덧칠하기, Time: 2.64 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,11 +1,11 @@
 function solution(n, m, section) {
-    let result = 0;
+    let answer = 0;
     let lastCovered = 0;
     for (let i=0;i<section.length;i++) {
-        if (lastCovered<section[i]){
-            result++
-            lastCovered=section[i]+m-1
+        if (lastCovered<section[i]) {
+            answer++
+            lastCovered = section[i]+m-1
         }
     }
-    return result
+    return answer
 }
