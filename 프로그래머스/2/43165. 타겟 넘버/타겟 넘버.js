1d7d665b1ac88b26c5466705fed4111fe8f91283
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-13 14:12:52 +0900

    [level 2] Title: 타겟 넘버, Time: 18.74 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,17 +1,15 @@
 function solution(numbers, target) {
     let answer = 0;
-    let stack = [[0,0]];
-    
-    while(stack.length>0) {
-        let [index,currentSum] = stack.pop();
+    const dfs = (index,currentSum)=>{
         if (index===numbers.length) {
             if (currentSum===target) {
                 answer++
             }
-            continue;
+            return;
         }
-        stack.push([index+1,currentSum+numbers[index]]);
-        stack.push([index+1,currentSum-numbers[index]])
+        dfs(index+1,currentSum+numbers[index]);
+        dfs(index+1,currentSum-numbers[index])
     }
+    dfs(0,0)
     return answer
 }
