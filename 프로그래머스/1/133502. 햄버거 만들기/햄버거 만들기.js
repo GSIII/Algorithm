1d9ed9fd651e3bd429d74546e3931924c8343941
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-13 17:25:53 +0900

    [level 1] Title: 햄버거 만들기, Time: 210.00 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,19 +1,22 @@
 function solution(ingredient) {
-    let result = 0;
-    let stack = [];
+    let answer = 0;
+    const hamburger = [1,2,3,1];
+    
+    let stack=[];
     
     for (let i=0;i<ingredient.length;i++) {
-        stack.push(ingredient[i])
+        stack.push(ingredient[i]) 
         
-        if (stack.length >= 4 && 
-            stack.slice(-4).join('') === '1231') {
+        if (stack.length>=4 && stack.slice(-4).join('')==='1231') {
+            stack.pop();
+            stack.pop();
+            stack.pop();
+            stack.pop();
             
-            stack.pop();
-            stack.pop();
-            stack.pop();
-            stack.pop();
-            result++
+            answer++
         }
-    }
-    return result
+
+        }
+    return answer
+    
 }
