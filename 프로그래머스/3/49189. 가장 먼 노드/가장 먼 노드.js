9900c2b13b4481f5cba2d62350f70caf3b6e5d07
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-14 13:47:14 +0900

    [level 3] Title: 가장 먼 노드, Time: 75.79 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,27 +1,25 @@
 function solution(n, edge) {
-    const graph = Array.from(Array(n+1),()=>[])
-
-    
+    let graph = Array.from(Array(n+1),()=>[]);
     edge.forEach(([src,dest])=>{
         graph[src].push(dest);
         graph[dest].push(src)
     })
+    let queue = [1];
     
-    
-    const distance = Array(n+1).fill(-1)
+    let distance = Array(n+1).fill(-1)
     distance[1]=0
-    const queue = [1]
     
     while(queue.length>0) {
-        const node = queue.shift();
-        
+        let node = queue.shift()
         for (let neighbor of graph[node]) {
-            if (distance[neighbor]===-1) {
-                distance[neighbor] = distance[node]+1
+            if (distance[neighbor]==-1) {
+                distance[neighbor] = distance[node]+1;
                 queue.push(neighbor)
             }
         }
     }
-    const maxDistance = Math.max(...distance);
-    return distance.filter(el=>el===maxDistance).length
+    let maxDistance = Math.max(...distance)
+    return distance.filter(d=>d===maxDistance).length
+    
+    
 }
