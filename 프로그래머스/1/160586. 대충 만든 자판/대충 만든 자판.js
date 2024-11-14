e83bbc50e5f8f7da5cb5d3ef5be4701f6d8cf0b2
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-14 15:32:06 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.05 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -2,14 +2,12 @@ function solution(keymap, targets) {
     let minPress = {}
     keymap.forEach((key)=>{
         for (let i=0;i<key.length;i++) {
-            let char = key[i]
-            
+            let char = key[i];
             if (!minPress[char] || minPress[char]>i+1) {
                 minPress[char] = i+1
             }
         }
     })
-    
     return targets.map((target)=>{
         let press = 0;
         for (let i=0;i<target.length;i++) {
