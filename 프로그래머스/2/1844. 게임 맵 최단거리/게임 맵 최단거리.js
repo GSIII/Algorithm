c2e1fa24b1f1e3bed1826d7007c38fbc36667231
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-14 11:59:55 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 35.61 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -5,30 +5,25 @@ function solution(maps) {
         [1,0],
         [-1,0],
         [0,1],
-        [0,-1]    
+        [0,-1]
     ]
-    const visited = Array.from(Array(n),()=>Array(m).fill(false))
+    let visited = Array.from(Array(n),()=>Array(m).fill(false))
     visited[0][0] = true;
+    let queue = [[0,0,1]] // 현재 위치, 움직인 거리
     
-    
-    const queue = [[0,0,1]];
-    
-    while(queue.length>0) {
-        let [x,y,move] = queue.shift()
-        if (x===n-1 && y===m-1) return move
+    while (queue.length>0) {
+        let [x,y,move] = queue.shift();
+        if (x===n-1&&y===m-1) return move
         
-        directions.forEach(([dx,dy])=>{
-            const nx = dx+x;
-            const ny = dy+y;
+        directions.forEach(([dx,dy])=> {
+            let nx = dx+x;
+            let ny = dy+y;
             
-            if (nx>=0&&nx<n && ny>=0&&ny<m&&maps[nx][ny]===1&&!visited[nx][ny]) {
+            if (nx>=0 && nx<n && ny>=0 && ny<m && maps[nx][ny]===1 && !visited[nx][ny]) {
                 visited[nx][ny] = true;
                 queue.push([nx,ny,move+1])
             }
         })
-
-        
     }
     return -1
-    
 }
