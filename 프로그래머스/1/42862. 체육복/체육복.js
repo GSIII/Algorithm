ff798970163caa2dc3115bddf9c9121cc919202e
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-14 21:57:23 +0900

    [level 1] Title: 체육복, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,20 +1,17 @@
 function solution(n, lost, reserve) {
-    let realLost = lost.filter(l=>!reserve.includes(l)).sort((a,b)=>a-b)
-    let realReserve = reserve.filter(r=>!lost.includes(r)).sort((a,b)=>a-b)
+    let newLost = lost.filter(l=>!reserve.includes(l)).sort((a,b)=>a-b)
+    let newReserve = reserve.filter(r=>!lost.includes(r)).sort((a,b)=>a-b)
     
-    let answer = n-realLost.length;
-    
-    for (let i=0;i<realLost.length;i++) {
-        let lostNum = realLost[i]
-        for (let j=0;j<realReserve.length;j++) {
-            let reserveNum = realReserve[j]
-            
-            if (lostNum===reserveNum+1||lostNum===reserveNum-1) {
-                answer++;
-                realReserve[j]=-1
+    let student = n-newLost.length;
+    for (let i=0;i<newReserve.length;i++) {
+        for (let j=0;j<newLost.length;j++) {
+            if (newReserve[i]-1===newLost[j] || newReserve[i]+1===newLost[j]) {
+                student++;
+                newReserve[i] = -1;
+                newLost.splice(j,1)
                 break;
             }
         }
     }
-    return answer
+    return student
 }
