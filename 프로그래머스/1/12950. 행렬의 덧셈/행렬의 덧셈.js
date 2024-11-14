0e1d560d2e70557821dcc49f5ffabd8d7196a298
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-14 16:01:16 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.58 ms, Memory: 67.3 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,7 +1,7 @@
 function solution(arr1, arr2) {
-    return arr1.map((nums,idx1)=>{
-        return nums.map((num,idx2)=>{
-            return num+arr2[idx1][idx2]
+    return arr1.map((num1,index1)=>{
+        return num1.map((num2,index2)=>{
+            return num2+arr2[index1][index2]
         })
     })
 }
