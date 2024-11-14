3cdef4c512b9c29de5b2fc004376817f8ca4031c
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-14 13:38:02 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 5.51 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,17 +1,15 @@
 function solution(left, right) {
-    let answer = 0
+    let sum = 0;
     for (let i=left;i<=right;i++) {
-        let count = 0;
+        let count = 0
         for (let j=1;j<=right;j++) {
-            if (i%j===0) {
-                count++
-            }
+            if (i%j===0) count++
         }
         if (count%2===0) {
-            answer +=i
+            sum+=i
         } else {
-            answer -= i
+            sum-=i
         }
     }
-    return answer
+    return sum
 }
