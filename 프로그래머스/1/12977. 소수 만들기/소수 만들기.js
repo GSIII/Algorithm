78ace5e452bf1fc7c4b9a59509fc021225b401b7
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-14 20:03:29 +0900

    [level 1] Title: 소수 만들기, Time: 288.34 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,24 +1,22 @@
 function solution(nums) {
-let count = 0;
-
-    const isPrime = (number) => {
-        if (number < 2) return false;
-        for (let i = 2; i <number; i++) {
-            if (number % i === 0) return false;
+    let answer = 0;
+    const isPrime = (num) =>{
+        for (let i=2;i<num;i++) {
+            if (num%i===0) return false
         }
-        return true;
-    };
-
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            for (let k = j + 1; k < nums.length; k++) {
-                const sum = nums[i] + nums[j] + nums[k];
-                if (isPrime(sum)) {
-                    count++;
+        return true
+    }
+    
+    for (let i=0;i<nums.length;i++) {
+        for (let j=i+1;j<nums.length;j++) {
+            for (let k=j+1;k<nums.length;k++) {
+                let sum = nums[i]+nums[j]+nums[k];
+                if (isPrime(sum)){
+                    answer++
                 }
+
             }
         }
     }
-
-    return count;
+    return answer
 }
