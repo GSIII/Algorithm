214e17d81ca530653d610bda02a004cbf30bba59
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-14 22:46:10 +0900

    [level 1] Title: 숫자 짝꿍, Time: 767.13 ms, Memory: 210 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,33 +1,31 @@
 function solution(X, Y) {
-    let answer = []
-    let x = [...X];
-    let y = [...Y];
     let xMap = new Map();
     let yMap = new Map();
-    
-    x.forEach((num,cnt)=>{
-        xMap.set(num,(xMap.get(num)||0)+1)
+    let x = [...X];
+    let y = [...Y]
+    let answer = [];
+    x.forEach(el=>{
+        xMap.set(el,(xMap.get(el)||0)+1)
     })
-    y.forEach((num,cnt)=>{
-        yMap.set(num,(yMap.get(num)||0)+1)
+    
+    y.forEach(el=>{
+        yMap.set(el,(yMap.get(el)||0)+1)
     })
     
-    for (let [xNum,xCnt] of xMap) {
-        if (yMap.has(xNum)) {
-            let minCnt = Math.min(xCnt,yMap.get(xNum))
+    for (let [key,value] of xMap) {
+        if (yMap.has(key)) {
+            let minCnt = Math.min(value,yMap.get(key))
             
             for (let i=1;i<=minCnt;i++) {
-                answer.push(xNum)
+                answer.push(key)
             }
         }
     }
-    answer.sort((a,b)=>b-a);
-    if (answer.length===0) return '-1';
-    for (let i=0;i<answer.length;i++) {
-        if (answer[i]==='0') {
-            return '0'
-        } else {
-            return answer.join('')
-        }
+    answer.sort((a,b)=>b-a)
+    if (answer.length===0) return '-1'
+    for (let ans of answer) {
+        if (ans==='0') return '0'
+        return answer.join('')
     }
+    
 }
