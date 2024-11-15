bd422c1975281625d4e9d3e184d23f314e94dc34
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-15 18:05:51 +0900

    [level 1] Title: 문자열 나누기, Time: 5.43 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -1,23 +1,26 @@
 function solution(s) {
-    let count = 0;
-    let i=0;
-    while(i<s.length) {
-        let x = s[i];
-        let xCount = 1;
-        let otherCount = 0;
-        i++
-        
-        while(i<s.length) {
-            if (s[i]===x) {
-                xCount++
+    let answer = 0; // 분리된 문자열의 개수
+    let stack = []; // 현재 상태를 추적하는 스택
+
+    for (const char of s) {
+        // 스택이 비어있으면 새로운 문자열 시작
+        if (stack.length === 0) {
+            stack.push([char, 1]); // [문자, 개수]
+            answer++; // 새 문자열로 카운트
+        } else {
+            // 스택의 마지막 문자가 현재 문자와 같다면 개수 증가
+            if (stack[stack.length - 1][0] === char) {
+                stack[stack.length - 1][1]++;
             } else {
-                otherCount++
+                // 다른 문자라면 개수 감소 (x와 x가 아닌 문자 비교)
+                stack[stack.length - 1][1]--;
+                // 개수가 0이 되면 해당 문자열 종료
+                if (stack[stack.length - 1][1] === 0) {
+                    stack.pop();
+                }
             }
-            i++;
-            
-            if (xCount===otherCount) break;
         }
-        count++
     }
-    return count
+
+    return answer;
 }
