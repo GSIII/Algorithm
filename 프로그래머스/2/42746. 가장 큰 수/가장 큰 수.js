db054ced166676b745bac27fa45ef59c735f75ec
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-15 13:15:40 +0900

    [level 2] Title: 가장 큰 수, Time: 161.85 ms, Memory: 44.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,4 +1,5 @@
 function solution(numbers) {
-    let answer = numbers.map(el=>el.toString()).sort((a,b)=>(b+a)-(a+b)).join('')
-    return answer[0]==='0' ? '0' : answer
+    let numToStr = numbers.map(number=>number.toString())
+    numToStr.sort((a,b)=>(b+a)-(a+b))
+    return numToStr[0]==='0' ? '0' : numToStr.join('')
 }
