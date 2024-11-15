ab29064e74bada7192b9a671032fd87f35682e65
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-15 12:23:47 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 39.63 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,9 +1,9 @@
 function solution(arr)
 {
-    let stack = [];
-    for (let num of arr) {
-        if (stack.length===0 || stack[stack.length-1]!==num) {
-            stack.push(num)
+    let stack = []
+    for (let i=0;i<arr.length;i++) {
+        if (stack.length===0 || stack[stack.length-1]!==arr[i]) {
+            stack.push(arr[i])
         }
     }
     return stack
