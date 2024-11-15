0d41004888b9cf7577ba67748ebbae3d6fad208f
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-15 17:45:26 +0900

    [level 1] Title: 숫자 짝꿍, Time: 748.77 ms, Memory: 209 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,31 +1,31 @@
 function solution(X, Y) {
+    let x = [...X];
+    let y = [...Y];
+    
     let xMap = new Map();
     let yMap = new Map();
-    let x = [...X];
-    let y = [...Y]
-    let answer = [];
-    x.forEach(el=>{
-        xMap.set(el,(xMap.get(el)||0)+1)
-    })
     
-    y.forEach(el=>{
-        yMap.set(el,(yMap.get(el)||0)+1)
+    let answer = [];
+    
+    x.forEach(num=>{
+        xMap.set(num,(xMap.get(num)||0)+1)
+    })
+    y.forEach(num=>{
+        yMap.set(num,(yMap.get(num)||0)+1)
     })
     
     for (let [key,value] of xMap) {
         if (yMap.has(key)) {
             let minCnt = Math.min(value,yMap.get(key))
-            
             for (let i=1;i<=minCnt;i++) {
                 answer.push(key)
-            }
+            } 
         }
     }
-    answer.sort((a,b)=>b-a)
+    answer.sort((a,b)=>b-a);
     if (answer.length===0) return '-1'
-    for (let ans of answer) {
-        if (ans==='0') return '0'
+    for (let num of answer) {
+        if (num==='0') return '0'
         return answer.join('')
     }
-    
 }
