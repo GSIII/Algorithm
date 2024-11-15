8d96d7a605fa2b5eb7cc8ae8e69a5255ebc2b9f3
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-15 13:28:52 +0900

    [level 2] Title: 프로세스, Time: 0.61 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,14 +1,14 @@
 function solution(priorities, location) {
-    let queue = priorities.map((priority,index)=>({priority,index}))
     let answer = 0;
+    let queue = priorities.map((priority,index)=>({priority,index}))
     while(queue.length>0) {
-        let current = queue.shift();
-        if (queue.some(process=>process.priority>current.priority)) {
-            queue.push(current)    
+        let current = queue.shift()
+        if (queue.some(process=>process.priority>current.priority)){
+            queue.push(current)
         } else {
-            answer++
+            answer++;
             
-            if (location===current.index) {
+            if (current.index===location) {
                 return answer
             }
         }
