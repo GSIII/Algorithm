1600e609c0e9fab2ba34e550cd3d58fc2c229df2
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-15 16:16:41 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 58.42 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,12 +1,10 @@
 function solution(participant, completion) {
     let map = new Map();
     for (let i=0;i<participant.length;i++) {
-        map.set(participant[i],(map.get(participant[i])||0)+1)
-        map.set(completion[i],(map.get(completion[i])||0)-1)
+        map.set(participant[i],(map.get(participant[i])||0)+1);
+        map.set(completion[i],(map.get(completion[i])||0)-1);
     }
     for (let [key,value] of map) {
-        if (value>0) {
-            return key
-        }
+        if (value>0) return key
     }
 }
