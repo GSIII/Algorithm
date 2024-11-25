ae64081f9eec932006b689d1de896dad38ebbefa
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2024-11-25 16:50:31 +0900

    Time: 6 ms (50.79%), Space: 52.8 MB (5.23%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    let map = new Map();
     
-    let arr = []
     for (let i=0;i<nums.length;i++) {
-        for (let j=i+1;j<nums.length;j++) {
-            if (nums[i]+nums[j]===target) {
-                arr.push(i,j)
-            }
+        map.set(nums[i],i)
+    }
+    
+    for (let i=0;i<nums.length;i++) {
+        let complement = target-nums[i]
+        if (map.has(complement)&&i!==map.get(complement)) {
+            return [i,map.get(complement)]
         }
     }
-    return arr
+    return []
 };
