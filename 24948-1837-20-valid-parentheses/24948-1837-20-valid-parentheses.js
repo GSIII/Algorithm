33903b158b124e9051bec994798c7e6fa2b7f6dc
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2025-01-02 13:23:45 +0900

    Time: 4 ms, Memory: 51.4 MB - LeetHub

---
+++ 24948-1837-20-valid-parentheses/24948-1837-20-valid-parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = [];
+    let map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        if (map[char]) {
+            stack.push(char);
+        } else {
+            if (stack.length === 0 || map[stack.pop()] !== char) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
