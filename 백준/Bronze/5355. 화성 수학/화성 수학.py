1adcea7b0cec2672ad6465fccd847e5c0cc80948
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2025-09-16 15:22:52 +0900

    [Bronze II] Title: 화성 수학, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5355. 화성 수학/화성 수학.py
+++ 백준/Bronze/5355. 화성 수학/화성 수학.py
@@ -1,10 +1,10 @@
 T = int(input())
 for i in range(0,T):
-    arr = list(map(str,input().split()))
+    arr = input().split()
     num = float(arr[0])
+    
     for j in range(1,len(arr)):
         if arr[j] == '@' : num *= 3
         elif arr[j] == '%' : num += 5
         elif arr[j] == '#' : num -= 7
-        
     print(f'{num:.2f}')
