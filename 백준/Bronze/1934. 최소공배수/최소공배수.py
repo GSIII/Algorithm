ce54db3055812e7e513d6c6aa75f194647ac44ad
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2025-09-17 11:38:11 +0900

    [Bronze I] Title: 최소공배수, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,16 +1,12 @@
 def gcd(a,b):
-    while b > 0:
+    while b != 0:
         a,b = b, a%b
     return a
 
 T = int(input())
-
 for _ in range(T):
     a,b = map(int,input().split())
-    
-    divisor = gcd(a,b)
-    
-    lcm = (a*b) // divisor
-    
-    print(lcm)
-         
+    num1 = gcd(a,b)
+    result = num1 * (a//num1) * (b//num1)
+    print(result)
+        
