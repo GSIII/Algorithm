6575ed46739a9a39c18d58641b84cad40fdf051b
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2025-10-31 23:26:07 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+M = int(input())
+R = 1
+
+graph = [[] for _ in range(N+1)]
+
+visited = [False] * (N+1)
+
+count = 0
+
+for _ in range(M):
+    u,v = map(int,input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+    
+def dfs(v):
+    global count
+    
+    visited[v] = True
+    count += 1
+    
+    for neighbor in graph[v]:
+        if visited[neighbor] == False:
+            dfs(neighbor)
+            
+dfs(R)
+
+print(count-1)
