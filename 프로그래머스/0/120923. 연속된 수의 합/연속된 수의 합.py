261bc1295bf333cba00d2b6d6959c19f50aee9a0
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2025-11-01 12:48:59 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,9 +1,4 @@
 def solution(num, total):
     s = sum(range(num))
-    x = (total - s) // num
-    
-    answer = []
-    for i in range(num):
-        answer.append(x+i)
-    
-    return answer
+    x = (total-s) // num
+    return [x+i for i in range(num)]
