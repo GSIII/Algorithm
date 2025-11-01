24378bddd8a9895be2dd02402ad92d437e37c79c
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2025-11-01 12:07:47 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 512 ms, Memory: 70312 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -7,13 +7,14 @@ N,M,R = map(int,input().split())
 graph = [[] for _ in range(N+1)]
 
 count = 1
+
 visit_order = [0] * (N+1)
 
 for _ in range(M):
     u,v = map(int,input().split())
     graph[u].append(v)
     graph[v].append(u)
-
+    
 for i in range(1,N+1):
     graph[i].sort()
     
@@ -27,7 +28,6 @@ def dfs(v):
         if visit_order[neighbor] == 0:
             dfs(neighbor)
     
-        
 dfs(R)
 
 for i in range(1,N+1):
