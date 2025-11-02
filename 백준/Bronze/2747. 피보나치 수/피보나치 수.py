30d57ce81777bd4c4eb8eaea6bdb2f853fae89cd
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2025-11-02 20:47:01 +0900

    [Bronze II] Title: 피보나치 수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.py
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -6,12 +6,15 @@ n = int(input())
 dp = [0] * (n+1)
 
 def fibo(n):
+    if n == 1:
+        return 1
+    elif n == 0:
+        return 0
     
-    if n >= 1:
-        dp[1] = 1
-        
-    for i in range(2,n+1):
-        dp[i] = dp[i-1] + dp[i-2] 
-    print(dp[n])
+    if dp[n] != 0:
+        return dp[n]
     
-fibo(n)
+    dp[n] = fibo(n-1) + fibo(n-2)
+    return dp[n]
+    
+print(fibo(n))
