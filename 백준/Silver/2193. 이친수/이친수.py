7757ce76ae4ac9c612eb5266d71820e4c736b229
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2025-11-04 12:26:49 +0900

    [Silver III] Title: 이친수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.py
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -3,12 +3,12 @@ input = sys.stdin.readline
 
 N = int(input())
 
-D = [[0 for i in range(2)] for j in range(N+1)]
-
+D = [[0] * 2 for _ in range(N+1)]
 D[1][1] = 1
 D[1][0] = 0
 
-for i in range(2,N+1):
+for i in range(2, N+1):
     D[i][0] = D[i-1][0] + D[i-1][1]
     D[i][1] = D[i-1][0]
+    
 print(D[N][1] + D[N][0])
