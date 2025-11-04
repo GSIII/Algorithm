9eaa9d56dc7fa3aef37ad948ed5ec40bc1cea7fa
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2025-11-04 13:14:36 +0900

    [Silver III] Title: 수열, Time: 84 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,18 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-n,k = map(int,input().split())
-A = [0] + list(map(int,input().split()))
-S = [0] * (n+1)
+N,K = map(int,input().split())
+temp = [0]+list(map(int,input().split()))
+S = [0] * (N+1)
 result = 0
-max_temp = -float('inf') 
+max_temp = -float('inf')
 
-for i in range(1,n+1):
-    S[i] = S[i-1] + A[i]
-
-for i in range(k,n+1):
-    result = S[i] - S[i-k]
+for i in range(1,N+1):
+    S[i] = S[i-1] + temp[i]
     
-    if max_temp < result : max_temp = result
-
+for i in range(K,N+1):
+    result = S[i] - S[i-K]
+    
+    if max_temp <result:
+        max_temp = result
 print(max_temp)
