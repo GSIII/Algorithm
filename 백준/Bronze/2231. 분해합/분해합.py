6222802ebfad27a31ef9f7a4759087c25e7a444b
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2026-01-16 12:23:29 +0900

    [Bronze II] Title: 분해합, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -2,11 +2,12 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-result = 0
-start = max(1, n-(9*len(str(n))))
 
-for i in range(start, n):
-    digits = [int(d) for d in str(i)]
+start = max(1, n-(len(str(n))*9))
+
+result = 0
+for i in range(start,n):
+    digits = list(map(int, str(i)))
     if sum(digits) + i == n:
         result = i
         break
