ad6995f9e16c9360a458f3efaa4d6f52b53fe31e
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2026-01-29 00:25:27 +0900

    [Bronze I] Title: 색종이, Time: 1348 ms, Memory: 40032 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,17 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-
 paper = [[0 for _ in range(1001)] for _ in range(1001)]
 
+n = int(input())
 for k in range(1,n+1):
     x,y,w,h = map(int,input().split())
-    for i in range(x,w+x):
-        for j in range(y,h+y):
-            paper[i][j] = k
-for k in range(1,n+1):
-    area = 0
-    for row in paper:
-        area += row.count(k)
-    print(area)
+    for j in range(y,y+h):
+        paper[j][x:x+w] = [k] * w
+            
+for p in range(1, n+1):
+    result = 0
+    for cnt in range(1001):
+        result += paper[cnt].count(p)
+    print(result)
+
+    
