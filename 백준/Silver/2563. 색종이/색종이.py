37858167f91a1defddeb070509fa5ecdd8725311
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2026-01-28 13:26:15 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+paper = [[0 for _ in range(100)] for _ in range(100)]
+
+for _ in range(n):
+    x,y = map(int,input().split())
+    
+    for i in range(x,x+10):
+        for j in range(y,y+10):
+            paper[i][j] = 1
+            
+area = 0
+for row in paper:
+    area += sum(row)
+print(area)
