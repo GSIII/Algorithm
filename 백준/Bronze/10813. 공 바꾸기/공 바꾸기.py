655b43dc626e741510ce57c7850034f421340fd6
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2026-01-28 21:47:33 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+n,m = map(int,input().split())
+balls = [x for x in range(1,n+1)]
+for _ in range(m):
+    i,j = map(int,input().split())
+    i -= 1
+    j -= 1
+    balls[i], balls[j] = balls[j], balls[i]
+    
+print(*balls)
