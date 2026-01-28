9e58dcbc3450a011291cbf77a1fe570c9f307297
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2026-01-29 01:35:42 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,18 +1,15 @@
-import sys
-input = sys.stdin.readline
+numbers = [list(map(int,input().split())) for _ in range(9)]
 
-board = [list(map(int,input().split())) for _ in range(9)]
-max_num = board[0][0]
-max_row = 1
+max_num = numbers[0][0]
 max_col = 1
-
-for i in range(len(board)):
-    for j in range(len(board[i])):
-        if board[i][j] > max_num:
-            max_num = board[i][j]
-            max_row = i
-            max_col = j
-            
+max_row = 1
+for i in range(9):
+    for j in range(9):
+        if numbers[i][j] > max_num:
+            max_num = numbers[i][j]    
+            max_row = i+1
+            max_col = j+1
+        
 print(max_num)
-print(max_row+1)
-print(max_col+1)
+print(max_row)
+print(max_col)
