456f5868bcb03748f69963bed8013e57021d9d64
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2026-01-29 01:24:48 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,8 +1,4 @@
-import sys
-input = sys.stdin.readline
-
 text = [list(input().strip()) for _ in range(5)]
-
 result = ''
 
 max_len = max([len(word) for word in text])
@@ -11,5 +7,5 @@ for col in range(max_len):
     for row in range(5):
         if col < len(text[row]):
             result += text[row][col]
-        
+
 print(result)
