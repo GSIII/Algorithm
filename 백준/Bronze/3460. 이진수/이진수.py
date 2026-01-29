0fe7ea39af596e7d49fffbf2eba83e4b8e090599
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2026-01-30 00:28:56 +0900

    [Bronze III] Title: 이진수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3460. 이진수/이진수.py
+++ 백준/Bronze/3460. 이진수/이진수.py
@@ -1,12 +1,13 @@
 t = int(input())
+
 for _ in range(t):
     n = int(input())
     binary = bin(n)[2:]
     binary = binary[::-1]
     
     position = []
-    for i, bit in enumerate(binary):
+    for i,bit in enumerate(binary):
         if bit == '1':
             position.append(str(i))
+    print(' '.join(position))
             
-    print(" ".join(position))   
