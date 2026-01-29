bd6c6c03d65aa2a5919ed7e45de2dfa47aba7145
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2026-01-29 20:23:14 +0900

    [Bronze I] Title: 더하기 사이클, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -0,0 +1,15 @@
+n = int(input())
+new_num = n
+cnt = 0
+
+while True:
+    cnt += 1
+    a = new_num // 10
+    b = new_num % 10
+    c = (a+b) % 10
+    
+    new_num = 10 * b + c
+    
+    if new_num == n:
+        break
+print(cnt)
