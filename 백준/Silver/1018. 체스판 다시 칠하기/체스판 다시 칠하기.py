e21e052ba39910c8c5bc228797479b48850d80eb
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2026-01-29 16:50:59 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -11,7 +11,8 @@ for i in range(n-7):
         for x in range(8):
             for y in range(8):
                 current = board[x+i][y+j]
-                
+                # (0,0)에서 (x,y)로 이동하는 과정을 생각하면 한번 이동할때마다(x or y) 색이 반전, 두번 이동하면(x+y) 다시 원래 색
+                # 즉 이동횟수가 짝수이면 시작점과 같은 색, 이동횟수 홀수이면 시작점과 반대 색 
                 if (x+y) % 2 == 0:
                     if current != 'W':
                         repaint_w += 1
@@ -25,3 +26,4 @@ for i in range(n-7):
         answer = min(answer, repaint_w, repaint_b)
                         
 print(answer)
+
