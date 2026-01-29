1b9d0717c62df5228f9d23bf65810a7f56966925
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2026-01-29 23:38:32 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,7 +1,7 @@
-import sys
-input = sys.stdin.readline
 cro = ['c=','c-','dz=','d-','lj','nj','s=','z=']
 text = input().strip()
+
 for c in cro:
     text = text.replace(c,'*')
+    
 print(len(text))
