b5d700cd7a27b0820fbad294ae1edd892ba39b6c
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2026-01-30 18:11:08 +0900

    [Bronze III] Title: 수학적 호기심, Time: 3056 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
@@ -1,11 +1,15 @@
+import sys
+input = sys.stdin.readline
+
 t = int(input())
 
+result = []
+
 for _ in range(t):
-    cnt = 0
     n,m = map(int,input().split())
+    cnt = 0
     for a in range(1,n):
         for b in range(a+1,n):
             if (a*a + b*b + m) % (a*b) == 0:
                 cnt += 1
-                
     print(cnt)
