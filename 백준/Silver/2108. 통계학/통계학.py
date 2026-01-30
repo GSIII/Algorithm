7d28407efbc00a064996f027b43acdffca5f9dd2
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2026-01-31 01:46:25 +0900

    [Silver II] Title: 통계학, Time: 364 ms, Memory: 55256 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -5,24 +5,23 @@ from collections import Counter
 n = int(input())
 numbers = [int(input()) for _ in range(n)]
 
-mean = round(sum(numbers)/n)
+mean = round(sum(numbers)/len(numbers))
 
 numbers.sort()
-median = numbers[n//2]
+median = numbers[len(numbers)//2]
 
 counter = Counter(numbers)
 max_freq = max(counter.values())
-modes = [num for num, freq in counter.items() if freq == max_freq]
+modes = [num for num,freq in counter.items() if freq == max_freq]
 modes.sort()
-
 if len(modes) > 1:
     mode = modes[1]
 else:
     mode = modes[0]
     
-range_val = max(numbers) - min(numbers)
+range_num = max(numbers) - min(numbers)
 
 print(mean)
 print(median)
 print(mode)
-print(range_val)
+print(range_num)
