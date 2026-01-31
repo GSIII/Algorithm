85837982ec34744832927237b9c9ab67e35aadb2
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2026-02-01 01:30:24 +0900

    [Silver I] Title: 절댓값 힙, Time: 124 ms, Memory: 42144 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -7,8 +7,8 @@ abs_heap = []
 for _ in range(n):
     x = int(input())
     
-    if x != 0:
-        heapq.heappush(abs_heap, (abs(x),x))
+    if x !=0:
+        heapq.heappush(abs_heap,(abs(x),x))
     else:
         if not abs_heap:
             print(0)
