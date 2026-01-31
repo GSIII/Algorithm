94fe51341fa3adbda08bd902be4154775b1d1621
Gal SeoIn <103163277+GSIII@users.noreply.github.com>
2026-02-01 00:35:36 +0900

    [Silver V] Title: 셀프 넘버, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,11 +1,9 @@
 max_num = 10000
-is_selfNum = [False] * 10001
+is_selfnum = [False] * 10001
 for n in range(1,max_num+1):
     d = n + sum(map(int,str(n)))
-    if d<=max_num:
-        
-        is_selfNum[d] = True
-    
+    if d <= max_num:
+        is_selfnum[d] = True
 for i in range(1, max_num+1):
-    if not is_selfNum[i]:
+    if not is_selfnum[i]:
         print(i)
